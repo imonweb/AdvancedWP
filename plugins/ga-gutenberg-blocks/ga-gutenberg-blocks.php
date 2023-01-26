4978760b217e85c754d2411be037a32b6c47e19f
Imon <contact@imonweb.co.uk>
2023-01-26 23:04:16 +0000

    ga-gutenberg-blocks setup

---
+++ plugins/ga-gutenberg-blocks/ga-gutenberg-blocks.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Plugin Name: Gourmet Artist Gutenberg Blocks
+ * Plugin URI: https://github.com/imonweb/ga-gutenberg-blocks
+ * Description: Gourmet Artist Gutenberg Blocks
+ * Author: Imon
+ * Author URI: https://www.imonweb.co.uk
+ * Text-Domain: ga-gutenberg-blocks
+ * Version: 0.1.0
+ * License: GPL2
+ * License URL: https://www.gnu.org/licenses/gpl-2.0.txt
+ * text-domain: ga-gutenberg-blocks
+ **/
+
+ /*  Prevent the execution */
